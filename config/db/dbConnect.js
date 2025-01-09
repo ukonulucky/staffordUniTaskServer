1938f9fb3a19cbe56fd25264e838ac4df4b38100
ukonulucky <ukonulucky@gmail.com>
2025-01-09 05:43:09 +0100

    auth

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -8,10 +8,28 @@ const dbConnetFunc = async() => {
  const res =    await mongoose.connect(MONGODB_URL, {
          useUnifiedTopology: true,
          useNewUrlParser: true,
-    })
+ })
+      
+      
+       // Log a successful connection
+    console.log('MongoDB connected successfully');
+
+    // You can also add event listeners to monitor MongoDB connection status
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose default connection is open');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.log(`Mongoose default connection has occurred ${err} error`);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('Mongoose default connection is disconnected');
+    });
+      
     return res
   } catch (error) {
-    console.log("new error")
+    console.error(`MongoDB connection failed: ${error.message}`);
    throw new Error(`MongoDb error: ${error.message}`)
   }
 }
