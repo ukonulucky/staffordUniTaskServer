ce7fe3a72057060a76677c1182b0d03be4aa6070
ukonulucky <ukonulucky@gmail.com>
2025-01-13 08:01:52 +0100

    admin

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 // cors configuration
 
 const corsOptions = {
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173",process.env.SERVER_URL],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
 }
